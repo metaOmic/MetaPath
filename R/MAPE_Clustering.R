c993c15e9adac6a2efa1b55fd310aac538085974
XiangruiZeng <XIZ120@pitt.edu>
2018-04-17 21:41:02 -0400

    Update MAPE_Clustering.R

--- R/MAPE_Clustering.R
+++ R/MAPE_Clustering.R
@@ -76,7 +76,7 @@ MAPE.Clustering <- function(summary,Num_Clusters = 3, kappa.result = kappa.resul
 #  }
   k = max(results2)
   if (k != k_org){warning(paste('Due to empty cluster(s), the cluster number of reduced from',
-                               k_org - 1, 'to', k-1)}
+                               k_org - 1, 'to', k-1))}
   pdf(paste(output_clustering,"/silhouette_plot.pdf",sep=""))
   plot(sil, nmax= 80, cex.names=0.6)
   dev.off()

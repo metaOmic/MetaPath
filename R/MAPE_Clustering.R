afba51edb58223fbee69304f3ccd4f9f28b21095
XiangruiZeng <XIZ120@pitt.edu>
2018-04-17 20:57:44 -0400

    Update MAPE_Clustering.R

--- R/MAPE_Clustering.R
+++ R/MAPE_Clustering.R
@@ -77,8 +77,7 @@ MAPE.Clustering <- function(summary,Num_Clusters = 3, kappa.result = kappa.resul
   results2<-c(results2,singleton)
   results2 <- results2[order(results2)]
   if (!all(1:k %in% unique(results$clustering))){
-    stop('Please choose a smaller number of clusters k or 
-          choose a larger FDR cutoff to select more pathways for clustering')
+    stop('Please choose a smaller number of clusters k or choose a larger FDR cutoff to select more pathways for clustering')
   }
   
   
